9b42eb88d6abe57c8e7535c5703013dbe149e13f
Vanesa Jones <vanessajonesb@gmailc.com>
2024-10-05 14:53:33 -0400

    Initial Commit

---
+++ SwiftUI-health/SwiftUI_healthApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_healthApp.swift
+//  SwiftUI-health
+//
+//  Created by mai pham on 10/5/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_healthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
